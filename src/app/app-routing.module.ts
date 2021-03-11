3fc4f2315b7a06e8a13144cafda6c32a269de031
Mason Adrales <masonadrales@Masons-MacBook-Pro-4.local>
2021-03-11 14:53:54 -0600

    Removed tabs routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
