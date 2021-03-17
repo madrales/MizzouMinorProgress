6231e686255b56961de1cf9196b56746221b832b
Mason Adrales <masonadrales@mwc-104023.dhcp.missouri.edu>
2021-03-17 13:37:45 -0500

    Updated branch

--- src/app/pages/all-courses/all-courses.page.ts
+++ src/app/pages/all-courses/all-courses.page.ts
@@ -6,16 +6,18 @@ import { Course } from 'src/app/interfaces/course';
 //importing service for completed IT courses
 import { CompletedITService } from 'src/app/services/completed-it.service'; 
 
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-all-courses',
   templateUrl: './all-courses.page.html',
   styleUrls: ['./all-courses.page.scss'],
 })
 export class AllCoursesPage implements OnInit {
-
+  private todo : FormGroup;
   data: Array<Course>; 
 
-  constructor(private model: CompletedITService) {
+  constructor(private model: CompletedITService, private formBuilder: FormBuilder) {
     this.data = model.getData(); 
    }
 
