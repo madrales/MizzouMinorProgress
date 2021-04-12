1aca8a0528ccd0c2c968b1ff05849ee942c9fa9c
Mason Adrales <masonadrales@mwc-106141.dhcp.missouri.edu>
2021-04-12 15:04:49 -0500

    Added CS minor page

--- src/app/pages/minors/compsci/compsci-routing.module.ts
+++ src/app/pages/minors/compsci/compsci-routing.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { MinorsPage } from './minors.page';
+import { CompsciPage } from './compsci.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: MinorsPage
-  },
-  {
-    path: 'compsci',
-    loadChildren: () => import('./compsci/compsci.module').then( m => m.CompsciPageModule)
+    component: CompsciPage
   }
 ];
 
@@ -18,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class MinorsPageRoutingModule {}
+export class CompsciPageRoutingModule {}
